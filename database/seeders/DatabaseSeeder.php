b6a62fcd327f9bb2d4c8e964f0fe7e0d9e312811
--global <maksim.garkavenkov@gmail.com>
2022-12-14 19:34:40 +0200

    Add Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,16 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\District;
+use App\Models\Community;
+use App\Models\WorkPlace;
+use App\Models\CouncilType;
+use App\Models\LandPlotType;
+use App\Models\HouseholdType;
+// use App\Models\SettlementType;
+use App\Models\FamilyRelationship;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RegionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +29,19 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            // UserSeeder::class,
+            SettlementTypeSeeder::class,
+            CouncilTypeSeeder::class,
+            HouseholdTypeSeeder::class,
+            LandPlotTypeSeeder::class,
+            SettlementTypeSeeder::class,
+            WorkPlaceSeeder::class,
+            FamilyRelationshipSeeder::class,
+            RegionSeeder::class,
+            DistrictSeeder::class,
+            CommunitySeeder::class,
+        ]);
+
     }
 }
