396e2142c16330ebed6db4170387c2a326508d4c
--global <maksim.garkavenkov@gmail.com>
2022-12-16 14:26:52 +0200

    Add Council database seeder

--- database/seeders/CouncilSeeder.php
+++ database/seeders/CouncilSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Region;
+use App\Models\Council;
+use App\Models\District;
+use App\Models\Community;
+use App\Models\CouncilType;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CouncilSeeder extends Seeder
 {
@@ -14,6 +20,25 @@ class CouncilSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Council::truncate();
+
+        $type = CouncilType::where('name', 'Сільська рада')->first();
+
+        $district = District::where('name', 'Конотопський район')
+                        ->where('region_id', Region::where('name', 'Сумська область')->first()->id)
+                        ->first();
+
+        $community = Community::where('name', 'Попівська громада')->where('district_id', $district->id)->first();
+
+        DB::table('councils')->insert([
+            [
+                'community_id'      =>  $community->id,
+                'council_type_id'   =>  $type->id,
+                'name'              =>  'Шаповалівська сільська рада',
+                'address'           =>  '41660, Сумська обл., Конотопський р-н, с. Шаповалівка, площа Козацької слави, буд. 9',
+                'edrpou'            =>  '04389288',
+                'koatuu'            =>  '5922088701',
+            ]
+        ]);
     }
 }
