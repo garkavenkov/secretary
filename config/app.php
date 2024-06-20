5da13eab6bff2fa297276d73a5eb600f7b193795
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2024-06-20 13:42:11 +0300

    Fix bug when generated dummy data for MySQL in DummyDataSeeder class

--- config/app.php
+++ config/app.php
@@ -40,7 +40,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '0.37.3',
+    'version' => '0.37.4',
 
     /*
     |--------------------------------------------------------------------------
