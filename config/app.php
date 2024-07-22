e04fed388915c26663c928ea8deff8ac986e4481
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2024-07-22 16:13:06 +0300

    Production build v0.41.0

--- config/app.php
+++ config/app.php
@@ -40,7 +40,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '0.40.1',
+    'version' => '0.41.0',
 
     /*
     |--------------------------------------------------------------------------
